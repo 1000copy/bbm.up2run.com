9417a52e470f521c6dda6299f81472b96cf89800
1000copy <1000copy@gmail.com>
2013-03-27 11:59:53 +0800

    multi user login

--- config.inc.php
+++ config.inc.php
@@ -15,4 +15,29 @@
         	echo mysql_error();
 		}
 	}
+		// $sql = "SHOW TABLES FROM `$database`";
+		// 	$result = mysql_query($sql);
+		// 	if (mysql_num_rows($result) > 0) {
+		// 		echo "<p>Available tables:</p>\n";
+		// 		echo "<pre>\n";
+		// 		while ($row = mysql_fetch_row($result)) {
+		// 			echo "{$row[0]}\n";
+		// 		}
+		// 		echo "</pre>\n";
+		// 	} else {
+		// 		echo "<p>The database '" . $database . "' contains no tables.</p>\n";
+		// 		echo mysql_error();
+		// 	}
+	// return result
+	function db_query($sql){
+		return mysql_query($sql);
+	}
+	// return int : row count
+	function db_row_count( $ds ){
+		return mysql_num_rows($ds);
+	}
+	// return row
+	function db_fetch_row( $ds ){
+		return mysql_fetch_row($ds);
+	}
 ?>
--- login.php
+++ login.php
@@ -1,10 +1,46 @@
 <?
 	session_start(); 
-	$_SESSION['user_id'] = 1 ;
-	$_SESSION['user_name'] = '1000copy';
 ?>
+<?
+	include "config.inc.php";
+	$action = $_GET['action'];
+	if ($action=="login"){
+		$user_id = $_POST['users'];
+		$result = db_query ("select fullname from user where id=${user_id}");
+		if (!$result){
+			die (mysql_error());
+		}
+		$row = db_fetch_row($result);
+		$_SESSION['user_id'] = $user_id ;
+		$_SESSION['user_name'] = $row[0];
+		header("Location:/book_list.php");
+	}else{
+		$result = db_query ("select id,fullname from user ");
+		if (!$result){
+			die (mysql_error());
+		}
+		if(db_row_count($result) == 0 )
+		{
+			exit ;
+		}
+	}
+?>
+
 <html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+</head>
 <body>
 	<? echo "login user id :" . $_SESSION['user_name'] ?>
+	<form action="<? echo $_SERVER['PHP_SELF'] ?>?action=login" method="post">
+	<select name = "users" >
+		<?
+		while($row = db_fetch_row($result)){// $row[0]
+		?>
+  			<option value="<? echo $row[0];?>"><?echo $row[1]?></option>
+  		<?}?>
+  	</select>
+  	<input type="submit"/>
+	</form>
 </body>
 </html>
